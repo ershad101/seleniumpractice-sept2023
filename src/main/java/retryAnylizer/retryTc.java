8fcf132001fdf753cebc84aa56bfeb2f415b8489
Dj-tech101 <dj-tech101@gmail.com>
2023-10-04 00:09:38 +0530

    commit log files

--- src/main/java/retryAnylizer/retryTc.java
+++ src/main/java/retryAnylizer/retryTc.java
@@ -7,17 +7,13 @@ public class retryTc implements IRetryAnalyzer {
 
 	@Override
 	public boolean retry(ITestResult result) {
-
 		int retry = 3;
 		int start = 0;
-
 		if (start < retry) {
 
 			start++;
 			return true;
-
 		}
-
 		return false;
 	}
 
