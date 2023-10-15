4941dd53002fdba43d06563d2a27dd80cdf7327b
Dj-tech101 <dj-tech101@gmail.com>
2023-10-15 16:48:10 +0530

    update test case 105

--- src/test/java/Demo_Sep_TestCases/demo_testcase_105.java
+++ src/test/java/Demo_Sep_TestCases/demo_testcase_105.java
@@ -1,26 +1,75 @@
 package Demo_Sep_TestCases;
 
-import org.apache.logging.log4j.Logger;
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.apache.poi.EncryptedDocumentException;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import BaseUtility.BaseClass;
+import ExcellUtilities.readExcel;
 import Loggings.LogerClass;
+import POMfile.HomePage;
+import POMfile.loginPage;
 import POMfile.selectHotel;
+import POMfile.serachHotelPage;
+import ReadProperties.PageProperty;
 
 public class demo_testcase_105 extends BaseClass {
 
-	selectHotel hotelselect;
-	
+	public selectHotel hotelselect;
+
+	public serachHotelPage searchHotel;
+
 	public Logger log;
 
-	public LogerClass obj;	
+	public LogerClass obj;
 
-	@Test(dependsOnGroups = "comman")
-	public void verify_date_time() {
+	public HomePage home;
 
-     
-		
+	public PageProperty pro;
+
+	public loginPage login;
+
+	public selectHotel selectHotel;
+
+	@Test(dependsOnGroups = "comman", dataProvider = "hotelDetails")
+	public void verify_date_time(String location, String hotel, String roomType, String numberOfRoom,
+			String checkinDate, String checkoutdate, String adultoption, String childrenO)
+			throws InterruptedException, IOException {
+
+		login = new loginPage(driver);
+		login.login();
+		log = LogerClass.getlogger();
+
+		log.info("validate the arrivaldate and depart date ");
+
 		hotelselect = new selectHotel(driver);
 
+		log.info("fill the neccessory data to search hotel ");
+
+		searchHotel = new serachHotelPage(driver);
+
+		searchHotel.hotelDetailsSubmission(location, hotel, roomType, numberOfRoom, checkinDate, checkoutdate,
+				adultoption, childrenO);
+
+		log.info("get the arrivadate as expectation ");
+
+		String expectationArrival_Date = hotelselect.getarrivaldate();
+
+		String expectedDepart_date = hotelselect.getdepartureDAte();
+
+	}
+
+	@DataProvider(name = "hotelDetails")
+	public String[][] hotelDetailsProvider() throws EncryptedDocumentException, IOException {
+
+		String path = ".\\testData\\SearchHotel\\hotelSearchDetails.xlsx";
+
+		readExcel xl = new readExcel(path, "Sheet1");
+
+		String data[][] = readExcel.dataContainer(path, "Sheet1");
+		return data;
 	}
 }
